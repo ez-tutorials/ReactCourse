12614bed494ffa2b1c265e407156781bc6059400
En Zhou <zhouen.nathan@gmail.com>
2019-01-23 10:06:42 +0200

    Completed section 3

--- components/src/index.js
+++ components/src/index.js
@@ -2,17 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'; // relative path
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            {/* Components are treated as JS tag */}
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <ApprovalCard>
+                <div>
+                    <h4> Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                {/* Components are treated as JS tag */}
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Yesterday at 4:45PM"
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Tomorrow at 4:45PM"
+                    content="Nice writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
